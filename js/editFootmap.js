e71cb4427024303ea98339624681429dd0af12d9
Allen <m1991allen@gmail.com>
2021-05-21 19:56:54 +0800

    210521

---
+++ js/editFootmap.js
@@ -0,0 +1,75 @@
+new Vue({
+    el: '#footmap',
+    data: {
+        // 北北基宜 照片路徑
+        photos_north1: [
+            './img/footmap/north1/1-3.jpg',
+            './img/footmap/north1/1.jpg',
+            './img/footmap/north1/1-1.jpg',
+            './img/footmap/north1/1-2.jpg',
+            './img/footmap/north1/2-6.jpg',
+            './img/footmap/north1/2-7.jpg',
+            './img/footmap/north1/2.jpg',
+            './img/footmap/north1/2-1.jpg',
+            './img/footmap/north1/2-2.jpg',
+            './img/footmap/north1/2-3.jpg',
+            './img/footmap/north1/2-4.jpg',
+            './img/footmap/north1/2-5.jpg',
+            './img/footmap/north1/3-1.jpg',
+            './img/footmap/north1/3.jpg',
+        ],
+
+        // 桃竹苗 照片路徑
+        photos_north2: [
+            './img/footmap/north2/1-10.jpg',
+            './img/footmap/north2/1-9.jpg',
+            './img/footmap/north2/1.jpg',
+            './img/footmap/north2/1-1.jpg',
+            './img/footmap/north2/1-2.jpg',
+            './img/footmap/north2/1-3.jpg',
+            './img/footmap/north2/1-4.jpg',
+            './img/footmap/north2/1-5.jpg',
+            './img/footmap/north2/1-6.jpg',
+            './img/footmap/north2/1-7.jpg',
+            './img/footmap/north2/1-8.jpg',
+            './img/footmap/north2/2.jpg',
+            './img/footmap/north2/2-1.jpg',
+            './img/footmap/north2/2-2.jpg',
+        ],
+
+        // 中彰雲嘉投 照片路徑
+        photos_center: [
+            './img/footmap/center/1-4.jpg',
+            './img/footmap/center/1-5.jpg',
+            './img/footmap/center/1.jpg',
+            './img/footmap/center/1-1.jpg',
+            './img/footmap/center/1-2.jpg',
+            './img/footmap/center/1-3.jpg',
+            './img/footmap/center/2-4.jpg',
+            './img/footmap/center/2-5.jpg',
+            './img/footmap/center/2.jpg',
+            './img/footmap/center/2-1.jpg',
+            './img/footmap/center/2-2.jpg',
+            './img/footmap/center/2-3.jpg',
+            './img/footmap/center/3.jpg',
+            './img/footmap/center/5.jpg',
+            './img/footmap/center/5-1.jpg',
+        ],
+
+        // 南高屏 照片路徑
+        photos_south: [
+            './img/footmap/south/1.jpg',
+            './img/footmap/south/2-3.jpg',
+            './img/footmap/south/2-4.jpg',
+            './img/footmap/south/2-5.jpg',
+            './img/footmap/south/2-6.jpg',
+            './img/footmap/south/2.jpg',
+            './img/footmap/south/2-1.jpg',
+            './img/footmap/south/2-2.jpg',
+            './img/footmap/south/3-3.jpg',
+            './img/footmap/south/3.jpg',
+            './img/footmap/south/3-1.jpg',
+            './img/footmap/south/3-2.jpg',
+        ],
+    }
+})
