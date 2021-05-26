ebdb7becbd957642883ed4cf0d2501b4b4e7bcff
Allen <m1991allen@gmail.com>
2021-05-26 14:42:56 +0800

    210526-2

--- js/editFootmap.js
+++ js/editFootmap.js
@@ -3,41 +3,38 @@ new Vue({
     data: {
         // 北北基宜 照片路徑
         photos_north1: [
+            './img/footmap/north1/新北0525-1.jpg',
             './img/footmap/north1/新北0524-1.jpg',
 
+            './img/footmap/north1/基0525-2.jpg',
+            './img/footmap/north1/基0525-1.jpg',
             './img/footmap/north1/基0524-1.jpg',
             './img/footmap/north1/基0523-4.jpg',
             './img/footmap/north1/基0523-3.jpg',
             './img/footmap/north1/基0523-2.jpg',
             './img/footmap/north1/基0523-1.jpg',
-
-            './img/footmap/north1/基0522-3.jpg',
-            './img/footmap/north1/基0522-2.jpg',
-            './img/footmap/north1/基0522-1.jpg',
         ],
 
         // 桃竹苗 照片路徑
         photos_north2: [
+            './img/footmap/north2/桃0525-2.jpg',
+            './img/footmap/north2/桃0525-1.jpg',
+
             './img/footmap/north2/桃0524-2.jpg',
             './img/footmap/north2/桃0524-1.jpg',
 
             './img/footmap/north2/桃0523-2.jpg',
             './img/footmap/north2/桃0523-1.jpg',
 
-            './img/footmap/north2/桃0522-4.jpg',
-            './img/footmap/north2/桃0522-3.jpg',
-            './img/footmap/north2/桃0522-2.jpg',
-            './img/footmap/north2/桃0522-1.jpg',
-
             './img/footmap/north2/竹縣0523-1.jpg',
             './img/footmap/north2/竹縣0522-1.jpg',
 
+            './img/footmap/north2/竹市0525-2.jpg',
+            './img/footmap/north2/竹市0525-1.jpg',
             './img/footmap/north2/竹市0523-2.jpg',
             './img/footmap/north2/竹市0523-1.jpg',
 
-            './img/footmap/north2/竹市0522-2.jpg',
-            './img/footmap/north2/竹市0522-1.jpg',
-
+            './img/footmap/north2/苗0525-1.jpg',
             './img/footmap/north2/苗0524-2.jpg',
             './img/footmap/north2/苗0524-1.jpg',
             './img/footmap/north2/苗0522-1.jpg',
@@ -45,69 +42,63 @@ new Vue({
 
         // 中彰雲嘉投 照片路徑
         photos_center: [
+            './img/footmap/center/中0525-2.jpg',
+            './img/footmap/center/中0525-1.jpg',
             './img/footmap/center/中0524-2.jpg',
             './img/footmap/center/中0524-1.jpg',
             './img/footmap/center/中0523-2.jpg',
             './img/footmap/center/中0523-1.jpg',
 
+            './img/footmap/center/彰0525-1.jpg',
             './img/footmap/center/彰0524-1.jpg',
             './img/footmap/center/彰0523-1.jpg',
-            './img/footmap/center/彰0522-1.jpg',
 
             './img/footmap/center/雲0523-1.jpg',
             './img/footmap/center/雲0522-1.jpg',
 
             './img/footmap/center/嘉市0523-1.jpg',
 
+            './img/footmap/center/投0525-3.jpg',
+            './img/footmap/center/投0525-2.jpg',
+            './img/footmap/center/投0525-1.jpg',
             './img/footmap/center/投0524-1.jpg',
             './img/footmap/center/投0523-1.jpg',
-            './img/footmap/center/投0522-1.jpg',
+
         ],
 
         // 南高屏 照片路徑
         photos_south: [
+            './img/footmap/south/南0525-8.jpg',
+            './img/footmap/south/南0525-7.jpg',
+            './img/footmap/south/南0525-6.jpg',
+            './img/footmap/south/南0525-5.jpg',
+            './img/footmap/south/南0525-4.jpg',
+            './img/footmap/south/南0525-3.jpg',
+            './img/footmap/south/南0525-2.jpg',
+            './img/footmap/south/南0525-1.jpg',
+
             './img/footmap/south/南0524-2.jpg',
-            './img/footmap/south/南0523-1.jpg',
+            './img/footmap/south/南0524-1.jpg',
 
-            './img/footmap/south/南0523-3.jpg',
-            './img/footmap/south/南0523-2.jpg',
-            './img/footmap/south/南0523-1.jpg',
-
-            './img/footmap/south/南0522-2.jpg',
-            './img/footmap/south/南0522-1.jpg',
+            './img/footmap/south/高0525-3.jpg',
+            './img/footmap/south/高0525-2.jpg',
+            './img/footmap/south/高0525-1.jpg',
 
             './img/footmap/south/高0524-4.jpg',
             './img/footmap/south/高0524-3.jpg',
             './img/footmap/south/高0524-2.jpg',
             './img/footmap/south/高0524-1.jpg',
 
-            './img/footmap/south/高0523-3.jpg',
-            './img/footmap/south/高0523-2.jpg',
-            './img/footmap/south/高0523-1.jpg',
-
-            './img/footmap/south/高0522-6.jpg',
-            './img/footmap/south/高0522-5.jpg',
-            './img/footmap/south/高0522-4.jpg',
-            './img/footmap/south/高0522-3.jpg',
-            './img/footmap/south/高0522-2.jpg',
-            './img/footmap/south/高0522-1.jpg',
+            './img/footmap/south/屏0525-5.jpg',
+            './img/footmap/south/屏0525-4.jpg',
+            './img/footmap/south/屏0525-3.jpg',
+            './img/footmap/south/屏0525-2.jpg',
+            './img/footmap/south/屏0525-1.jpg',
 
             './img/footmap/south/屏0524-4.jpg',
             './img/footmap/south/屏0524-3.jpg',
             './img/footmap/south/屏0524-2.jpg',
             './img/footmap/south/屏0524-1.jpg',
-
-            './img/footmap/south/屏0523-6.jpg',
-            './img/footmap/south/屏0523-5.jpg',
-            './img/footmap/south/屏0523-4.jpg',
-            './img/footmap/south/屏0523-3.jpg',
-            './img/footmap/south/屏0523-2.jpg',
-            './img/footmap/south/屏0523-1.jpg',
-
-            './img/footmap/south/屏0522-4.jpg',
-            './img/footmap/south/屏0522-3.jpg',
-            './img/footmap/south/屏0522-2.jpg',
-            './img/footmap/south/屏0522-1.jpg',
         ],
 
         // 宜花東 照片路徑
@@ -115,13 +106,14 @@ new Vue({
             './img/footmap/east/宜0523-1.jpg',
             './img/footmap/east/宜0522-1.jpg',
 
+            './img/footmap/east/花0525-1.jpg',
             './img/footmap/east/花0523-4.jpg',
             './img/footmap/east/花0523-3.jpg',
             './img/footmap/east/花0523-2.jpg',
             './img/footmap/east/花0523-1.jpg',
-            './img/footmap/east/花0522-3.jpg',
-            './img/footmap/east/花0522-2.jpg',
-            './img/footmap/east/花0522-1.jpg',
+
+            './img/footmap/east/東0525-2.jpg',
+            './img/footmap/east/東0525-1.jpg',
         ],
     },
 })
