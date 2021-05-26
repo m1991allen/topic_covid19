6121a480a1af026f9c821700daa3286ad9a1f745
Allen <m1991allen@gmail.com>
2021-05-26 18:51:08 +0800

    210526-4

--- js/editFootmap.js
+++ js/editFootmap.js
@@ -3,9 +3,11 @@ new Vue({
     data: {
         // 北北基宜 照片路徑
         photos_north1: [
+            './img/footmap/north1/新北0526-1.jpg',
             './img/footmap/north1/新北0525-1.jpg',
             './img/footmap/north1/新北0524-1.jpg',
 
+            './img/footmap/north1/基0526-1.jpg',
             './img/footmap/north1/基0525-2.jpg',
             './img/footmap/north1/基0525-1.jpg',
             './img/footmap/north1/基0524-1.jpg',
@@ -17,6 +19,8 @@ new Vue({
 
         // 桃竹苗 照片路徑
         photos_north2: [
+            './img/footmap/north2/桃0526-2.jpg',
+            './img/footmap/north2/桃0526-1.jpg',
             './img/footmap/north2/桃0525-2.jpg',
             './img/footmap/north2/桃0525-1.jpg',
 
@@ -26,6 +30,7 @@ new Vue({
             './img/footmap/north2/桃0523-2.jpg',
             './img/footmap/north2/桃0523-1.jpg',
 
+            './img/footmap/north2/竹縣0526-1.jpg',
             './img/footmap/north2/竹縣0523-1.jpg',
             './img/footmap/north2/竹縣0522-1.jpg',
 
@@ -34,6 +39,7 @@ new Vue({
             './img/footmap/north2/竹市0523-2.jpg',
             './img/footmap/north2/竹市0523-1.jpg',
 
+            './img/footmap/north2/苗0526-1.jpg',
             './img/footmap/north2/苗0525-1.jpg',
             './img/footmap/north2/苗0524-2.jpg',
             './img/footmap/north2/苗0524-1.jpg',
@@ -49,13 +55,17 @@ new Vue({
             './img/footmap/center/中0523-2.jpg',
             './img/footmap/center/中0523-1.jpg',
 
+            './img/footmap/center/彰0526-1.jpg',
             './img/footmap/center/彰0525-1.jpg',
             './img/footmap/center/彰0524-1.jpg',
             './img/footmap/center/彰0523-1.jpg',
 
+            './img/footmap/center/雲0526-2.jpg',
+            './img/footmap/center/雲0526-1.jpg',
             './img/footmap/center/雲0523-1.jpg',
             './img/footmap/center/雲0522-1.jpg',
 
+            './img/footmap/center/嘉市0526-1.jpg',
             './img/footmap/center/嘉市0523-1.jpg',
 
             './img/footmap/center/投0525-3.jpg',
@@ -63,11 +73,12 @@ new Vue({
             './img/footmap/center/投0525-1.jpg',
             './img/footmap/center/投0524-1.jpg',
             './img/footmap/center/投0523-1.jpg',
-
         ],
 
         // 南高屏 照片路徑
         photos_south: [
+            './img/footmap/south/南0526-2.jpg',
+            './img/footmap/south/南0526-1.jpg',
             './img/footmap/south/南0525-8.jpg',
             './img/footmap/south/南0525-7.jpg',
             './img/footmap/south/南0525-6.jpg',
@@ -84,11 +95,18 @@ new Vue({
             './img/footmap/south/高0525-2.jpg',
             './img/footmap/south/高0525-1.jpg',
 
+            './img/footmap/south/高0526-4.jpg',
+            './img/footmap/south/高0526-3.jpg',
+            './img/footmap/south/高0526-2.jpg',
+            './img/footmap/south/高0526-1.jpg',
             './img/footmap/south/高0524-4.jpg',
             './img/footmap/south/高0524-3.jpg',
             './img/footmap/south/高0524-2.jpg',
             './img/footmap/south/高0524-1.jpg',
 
+            './img/footmap/south/屏0526-3.jpg',
+            './img/footmap/south/屏0526-2.jpg',
+            './img/footmap/south/屏0526-1.jpg',
             './img/footmap/south/屏0525-5.jpg',
             './img/footmap/south/屏0525-4.jpg',
             './img/footmap/south/屏0525-3.jpg',
@@ -106,12 +124,16 @@ new Vue({
             './img/footmap/east/宜0523-1.jpg',
             './img/footmap/east/宜0522-1.jpg',
 
+            './img/footmap/east/花0526-2.jpg',
+            './img/footmap/east/花0526-1.jpg',
             './img/footmap/east/花0525-1.jpg',
             './img/footmap/east/花0523-4.jpg',
             './img/footmap/east/花0523-3.jpg',
             './img/footmap/east/花0523-2.jpg',
             './img/footmap/east/花0523-1.jpg',
 
+            './img/footmap/east/東0526-2.jpg',
+            './img/footmap/east/東0526-1.jpg',
             './img/footmap/east/東0525-2.jpg',
             './img/footmap/east/東0525-1.jpg',
         ],
