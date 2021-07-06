36d5bb67871ebb555e02018be23bd9f146b552cf
Allen <m1991allen@gmail.com>
2021-07-06 11:52:54 +0800

    210706

--- js/editFootmap.js
+++ js/editFootmap.js
@@ -1,32 +1,27 @@
 new Vue({
     el: '#footmap',
     data: {
-        // 北北基宜 照片路徑
+        // 北北基 照片路徑
         photos_north1: [
             './img/footmap/north1/基0624-1.jpg',
             './img/footmap/north1/基0614-1.jpg',
             './img/footmap/north1/基0612-1.jpg',
-            './img/footmap/north1/新北0601-1.jpg',
+            './img/footmap/north1/新北0705-1.jpg',
+            './img/footmap/north1/新北0704-1.jpg',
+            './img/footmap/north1/新北0630-1.jpg',
         ],
 
         // 桃竹苗 照片路徑
         photos_north2: [
-            './img/footmap/north2/桃0629-1.jpg',
-            './img/footmap/north2/桃0628-2.jpg',
-            './img/footmap/north2/桃0628-1.jpg',
-            './img/footmap/north2/桃0626-2.jpg',
-            './img/footmap/north2/桃0626-1.jpg',
-            './img/footmap/north2/桃0624-1.jpg',
-            './img/footmap/north2/桃0623-1.jpg',
-            './img/footmap/north2/桃0621-1.jpg',
-            './img/footmap/north2/竹市0612-1.jpg',
+            './img/footmap/north2/桃0705-1.jpg',
+            './img/footmap/north2/桃0703-2.jpg',
+            './img/footmap/north2/桃0703-1.jpg',
+            './img/footmap/north2/桃0701-1.jpg',
+            './img/footmap/north2/竹市0701-3.jpg',
+            './img/footmap/north2/竹市0701-2.jpg',
+            './img/footmap/north2/竹市0701-1.jpg',
             './img/footmap/north2/竹縣0629-1.jpg',
-            './img/footmap/north2/竹縣0625-2.jpg',
-            './img/footmap/north2/竹縣0625-1.jpg',
-            './img/footmap/north2/竹縣0624-2.jpg',
-            './img/footmap/north2/竹縣0624-1.jpg',
-            './img/footmap/north2/竹縣0623-1.jpg',
-            './img/footmap/north2/竹縣0620-1.jpg',
+            './img/footmap/north2/苗0705-1.jpg',
             './img/footmap/north2/苗0624-2.jpg',
             './img/footmap/north2/苗0624-1.jpg',
         ],
@@ -35,6 +30,7 @@ new Vue({
         photos_center: [
             './img/footmap/center/中0625-1.jpg',
             './img/footmap/center/中0620-1.jpg',
+            './img/footmap/center/彰0630-1.jpg',
             './img/footmap/center/彰0629-1.jpg',
             './img/footmap/center/彰0628-1.jpg',
             './img/footmap/center/彰0626-2.jpg',
@@ -64,6 +60,9 @@ new Vue({
             './img/footmap/south/南0627-3.jpg',
             './img/footmap/south/南0627-2.jpg',
             './img/footmap/south/南0627-1.jpg',
+            './img/footmap/south/高0701-1.jpg',
+            './img/footmap/south/高0630-2.jpg',
+            './img/footmap/south/高0630-1.jpg',
             './img/footmap/south/高0625-1.jpg',
             './img/footmap/south/高0624-3.jpg',
             './img/footmap/south/高0624-2.jpg',
@@ -86,8 +85,8 @@ new Vue({
 
         // 宜花東 照片路徑
         photos_east: [
-            './img/footmap/east/宜0623-1.jpg',
-            './img/footmap/east/宜0621-1.jpg',
+            './img/footmap/east/宜0704-1.jpg',
+            './img/footmap/east/宜0703-1.jpg',
             './img/footmap/east/花0617-1.jpg',
             './img/footmap/east/花0616-5.jpg',
             './img/footmap/east/花0616-4.jpg',
